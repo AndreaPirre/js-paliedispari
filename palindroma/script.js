7e588dfc99ec525f6db4e0d205f60b4c97fcf8a4
Andrea <andreapirrello1990@gmail.com>
2020-06-29 23:35:18 +0200

    esercizio palindroma con funzione

--- palindroma/script.js
+++ palindroma/script.js
@@ -1,29 +1,41 @@
-// Palidroma
-// Chiedere all’utente di inserire una parola
-// Creare una funzione per capire se la parola inserita è palindroma
+// 1__Scrivi una funzione per capire se la parola è palindroma
 
-// Inizio
+var parolaUtente = prompt("Inserisci una parola: ");
+var parolaGirata = roveciaParola(parolaUtente);
 
-// chiadiamo all'utente di inserire una parola
-var parola = prompt("Scrivi una parola: ");
-
-
-console.log(parola);
-
-var array = parola.split("");
-
-console.log(array);
-
-var strReverse = array.reverse();
-
-console.log(strReverse);
-
-var confronto = strReverse.join("")
-
-if (parola!=confronto) {
-    console.log("La parola non è palindroma");
-
-}else if (parola==confronto) {
-    console.log("La parola è palindroma");
-
+if (parolaUtente == parolaGirata) {
+ console.log("La parola è palindroma");
 }
+
+else {
+  console.log("La parola non è palindroma");
+}
+function roveciaParola(parola) {
+ var parolaRovesciata = "";
+ for (var i = parola.length -1; i >= 0; i--) {
+   parolaRovesciata += parola[i]
+ }
+ return parolaRovesciata;
+}
+
+// esercizio con uso di split, reverse e join
+
+// console.log(parola);
+//
+// var array = parola.split("");
+//
+// console.log(array);
+//
+// var strReverse = array.reverse();
+//
+// console.log(strReverse);
+//
+// var confronto = strReverse.join("")
+//
+// if (parola!=confronto) {
+//     console.log("La parola non è palindroma");
+//
+// }else if (parola==confronto) {
+//     console.log("La parola è palindroma");
+//
+// }
